744ab318965df250815d8886d5b14b54d1e66c07
Rafael Soto <rafael.soto.iv@gmail.com>
2021-04-04 21:17:47 -0500

    Added more plant animations

--- Pages/plant.js
+++ Pages/plant.js
@@ -1,10 +1,10 @@
 class Plant{
-  constructor(plant, string){
+  constructor(plant, string, cap){
     this.plant = plant;
     this.plant_name = string;
     this.frame = 0;
-    this.cap = 8;
-    this.level = "../Photos/" + this.plant_name + "/tile00" + this.frame + ".png";
+    this.cap = cap;
+    this.level = "../Photos/" + this.plant_name + "/tile" + this.frame + ".png";
     document.getElementById(this.plant_name).src = this.level;
   }
   frameup(){
@@ -12,20 +12,22 @@ class Plant{
     if (this.frame > this.cap){
       this.frame = this.cap;
     }
-    this.level = "../Photos/" + this.plant_name + "/tile00" + this.frame + ".png";
-    document.getElementById(this.plant_name).src = this.level
-    console.log(document.getElementById(this.plant_name).src);
+    this.level = "../Photos/" + this.plant_name + "/tile" + this.frame + ".png";
+    document.getElementById(this.plant_name).src = this.level;
+    console.log(document.getElementById(this.plant_name));
   }
   framedown(){
     this.frame -= 1;
     if(this.frame < 0){
-      this.frame = 0
+      this.frame = 0;
     }
-    this.level = "../Photos/" + this.plant_name + "/tile00" + this.frame + ".png";
+    this.level = "../Photos/" + this.plant_name + "/tile" + this.frame + ".png";
     document.getElementById(this.plant_name).src = this.level;
   }
 }
 
 function classify(){
-  plant1 = new Plant(plant1, 'plant1');
+  plant1 = new Plant(plant1, 'plant1', 8);
+  plant2 = new Plant(plant2, 'plant2', 14);
+  plant3 = new Plant(plant3, 'plant3', 21);
 }
